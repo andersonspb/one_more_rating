60d006249c616490cb13fcaa6abdff961468e5a7
Andrey Smirnov <anderson.smirnov@gmail.com>
2012-05-31 18:34:32 +0400

    Check if entity is rateable before try to rate it

--- app/controllers/one_more_rating/ratings_controller.rb
+++ app/controllers/one_more_rating/ratings_controller.rb
@@ -1,7 +1,11 @@
 module OneMoreRating
   class RatingsController < ApplicationController
     def create
-      @rateable = Object.const_get(params[:rateable_type]).find(params[:rateable_id])
+      rateable_type = Object.const_get(params[:rateable_type])
+      @rateable = rateable_type.find(params[:rateable_id])
+
+      raise SecurityError.new("An attempt to rate an object of type that is not rateable") if !@rateable.respond_to?(:rate)
+
       scope = @rateable.class.rateable_scope.call
       @rateable.rate(params[:score], current_user.id, scope)
 
